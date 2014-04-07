491feaa08728e20f31882f79be357837d565a4f9
Devin Barry <devinbarry@users.noreply.github.com>
2014-04-07 16:08:12 +1200

    Basic functionality including tests to parse the output from git into a dict containing references. Dict is ordered by date.

--- tests.py
+++ tests.py
@@ -3,6 +3,7 @@ __author__ = 'devinbarry@users.noreply.github.com'
 import unittest
 import datetime
 from work_tracker import _parse_line_into_date_and_comment, _parse_date_string_into_date
+from work_tracker import _get_refs_from_comment, _get_nums_from_comment, _get_pertinant_words
 
 
 class WorkTrackerTests(unittest.TestCase):
@@ -15,4 +16,34 @@ class WorkTrackerTests(unittest.TestCase):
     def test_parse_date_string_into_date(self):
         date_string = "Thu Nov 7 12:26:45 2013 +1300"
         date = _parse_date_string_into_date(date_string)
-        self.assertIsInstance(date, datetime.datetime)
+        self.assertIsInstance(date, datetime.date)
+
+    def test_get_refs_from_comment(self):
+        comment = "Bug fix in tests.ApiTest due to change in get_client. Refs #4390"
+        list_out = _get_refs_from_comment(comment)
+        self.assertEqual(list_out[0], 'Refs #4390')
+
+    def test_get_refs_from_comment_multi(self):
+        comment = "Bug fix in tests.ApiTest due to change in get_client. Refs #4390, Refs #2556"
+        list_out = _get_refs_from_comment(comment)
+        self.assertEqual(list_out[0], 'Refs #4390')
+        self.assertEqual(list_out[1], 'Refs #2556')
+
+    def test_get_refs_from_comment_multi_2(self):
+        comment = "Bug fix in Refs #4567tests.ApiTest due to change in get_client. Refs #4390, Refs #2556"
+        list_out = _get_refs_from_comment(comment)
+        self.assertEqual(list_out[0], 'Refs #4567')
+        self.assertEqual(list_out[1], 'Refs #4390')
+        self.assertEqual(list_out[2], 'Refs #2556')
+
+    def test_get_nums_from_comment(self):
+        comment = "Bug fix in tests.ApiTest due to change in get_client. Refs #4390"
+        list_out = _get_nums_from_comment(comment)
+        self.assertEqual(list_out[0], '4390')
+
+    def test_get_pertinant_words(self):
+        comment = "Bug fix in Refs 4567tests.ApiTest due to change in get_client. Refs #4390, Refs #2556"
+        list_out = _get_pertinant_words(comment)
+        self.assertEqual(list_out[0], 'Refs #4390')
+        self.assertEqual(list_out[1], 'Refs #2556')
+        self.assertEqual(list_out[2], '4567')
--- work_tracker.py
+++ work_tracker.py
@@ -1,20 +1,8 @@
 __author__ = 'devinbarry@users.noreply.github.com'
 
-
-if __name__ == '__main__':
-    venv = "source /Library/Frameworks/Python.framework/Versions/2.7/bin/virtualenvwrapper.sh"
-
-    f = open('devinbarry_git_log_output.txt', 'r')
-
-
-    count = 0
-    line = f.readline()
-    while line != '':
-        count += 1
-        line = f.readline()
-
-    print('Count: {}'.format(count))
-    f.close()
+import re
+from collections import OrderedDict
+from dateutil.parser import parse
 
 
 def _parse_line_into_date_and_comment(file_line):
@@ -26,4 +14,63 @@ def _parse_line_into_date_and_comment(file_line):
 
 
 def _parse_date_string_into_date(date_string):
-    return None
+    return parse(date_string).date()
+
+
+def _get_nums_from_comment(comment):
+    nums_out = re.findall(r'\d{4}', comment)
+    return nums_out
+
+
+def _get_refs_from_comment(comment):
+    list_out = re.findall(r'refs #\d{4}', comment, re.IGNORECASE)
+    return list_out
+
+
+def _remove_words_from_string(string, word_list):
+    new_word_list = set(word_list)
+    for word in new_word_list:
+        string = string.replace(word, '')
+    return string
+
+
+def _get_pertinant_words(comment):
+    refs = _get_refs_from_comment(comment)
+    new_comment = _remove_words_from_string(comment, refs)
+    # Probably very little will ever happen here
+    nums = _get_nums_from_comment(new_comment)
+    return refs + nums
+
+
+if __name__ == '__main__':
+    venv = "source /Library/Frameworks/Python.framework/Versions/2.7/bin/virtualenvwrapper.sh"
+
+    f = open('devinbarry_git_log_output.txt', 'r')
+
+    output = OrderedDict()
+
+
+    count = 0
+    line = f.readline()
+    while line != '':
+        count += 1
+        line = f.readline()
+
+        date_string, comment = _parse_line_into_date_and_comment(line)
+        date = _parse_date_string_into_date(date_string)
+        words = _get_pertinant_words(comment)
+        try:
+            output[date].extend(words)
+        except KeyError:
+            output[date] = list()
+            output[date].extend(words)
+
+    print('Processed {} lines'.format(count))
+    f.close()
+
+    for k in output.keys():
+        # remove dupes
+        output[k] = list(set(output[k]))
+
+    for k in output.keys():
+        print('{}: {}'.format(k, output[k]))
